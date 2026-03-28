974963bcf2ffbfb34ea95da2ce997e5786ed1f80
ctn-lima <claytonlima@live.com>
2026-03-28 19:46:50 -0300

    Subida de exercícios até o 21

--- exercícios/exercicio21.py
+++ exercícios/exercicio21.py
@@ -1,3 +1,7 @@
 #Faça um programa em python que abra e reproduza um arquivo de audio em MP3
 
-from pygame import 
+from pygame import mixer
+
+mixer.init()
+mixer.music.load('assets/media/sonican-thinking-time-ticking-power-223023.mp3')
+mixer.music.play
