dce566de7ee9731bb00562ac41105d01aedbfc12
ctn-lima <claytonlima@live.com>
2026-04-23 12:41:05 -0300

    Testes até o exercício 27

--- exercícios/exercicio26.py
+++ exercícios/exercicio26.py
@@ -3,5 +3,5 @@
 frase = input('Digite qualquer frase: ').strip()
 
 print('A frase "{}" possui {} letras A.'.format(frase.capitalize(), frase.upper().count('A')))
-print('A primeira ocorrência de aparição da letra A na frase está na posição {}.'.format(frase.upper().find('A')))
+print('A primeira ocorrência de aparição da letra A na frase está na posição {}.'.format(frase.upper().find('A')+1))
 print('A última ocorrência de aparidão da dela A na frase está na posição {}.'.format(frase.upper().rfind('A')))
